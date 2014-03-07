853c73dea2ebc400b7fd4aadb8cc92c31cf6e59f
Michele Locati <michele@locati.it>
2014-03-07 11:17:57 +0100

    Allow specify different databases

--- includes/configuration.sample.php
+++ includes/configuration.sample.php
@@ -4,6 +4,8 @@
 * @var string
 */
 define('_C5VT_VERSIONS_FOLDER', '');
+
+// Database configuration for "concrete5 versions tester"
 /** DB: server
 * @var string
 */
@@ -20,3 +22,22 @@ define('_C5VT_DB_PASSWORD', '');
 * @var string
 */
 define('_C5VT_DB_DATABASE', '');
+
+// Database configuration concrete5 runtime (optional, if omitted we'll use the DB for "concrete5 versions tester")
+/** DB: server
+ * @var string
+ */
+//define('_C5VT_DB_SERVER_C5', '');
+/** DB: username
+ * @var string
+*/
+//define('_C5VT_DB_USERNAME_C5', '');
+/** DB: password
+ * @var string
+*/
+//define('_C5VT_DB_PASSWORD_C5', '');
+/** DB: database name
+ * @var string
+*/
+//define('_C5VT_DB_DATABASE_C5', '');
+
