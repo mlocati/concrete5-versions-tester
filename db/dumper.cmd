e516b04b45acefa4f1965ff3d86a7393c4708323
Michele Locati <michele@locati.it>
2014-03-13 17:19:53 +0100

    More mysqldump path under windows

--- db/dumper.cmd
+++ db/dumper.cmd
@@ -1,2 +1,25 @@
 @echo off
-"C:\Program Files\MySQL\bin\mysqldump.exe" --no-defaults --host=localhost --user=root --password --skip-comments --add-drop-table --skip-allow-keywords --create-options --default-character-set=utf8 --disable-keys --no-create-db --no-data --skip-quote-names --set-charset --skip-dump-date --result-file=%~dp0dump.sql c5versionstester
+setlocal
+
+if exist "C:\Program Files\MySQL\bin\mysqldump.exe" (
+	set MYSQLDUMP=C:\Program Files\MySQL\bin\mysqldump.exe
+) else (
+	if exist "C:\Program Files\MySQL\Server\bin\mysqldump.exe" (
+		set MYSQLDUMP=C:\Program Files\MySQL\Server\bin\mysqldump.exe
+	) else (
+		echo mysqldump not found
+		goto err
+	)
+)
+"%MYSQLDUMP%" --no-defaults --host=localhost --user=root --password --skip-comments --add-drop-table --skip-allow-keywords --create-options --default-character-set=utf8 --disable-keys --no-create-db --no-data --skip-quote-names --set-charset --skip-dump-date --result-file=%~dp0dump.sql c5versionstester
+if errorlevel 1 (
+	echo mysqldump failed
+	goto err
+)
+goto done
+
+:err
+pause
+
+:done
+endlocal
