6c6d2db67d70e001011cee5a91b2d11f20ebe5ec
Michele Locati <michele@locati.it>
2014-03-14 16:08:07 +0100

    Update eraser

--- db/erase.sql
+++ db/erase.sql
@@ -1,6 +1,8 @@
 update _C5VT_Version set vConstantsParsed = 0, vFunctionsParsed=0, vHelpersParsed=0, vLibrariesParsed=0, vModelsParsed=0;
 delete from _C5VT_Method;
 alter table _C5VT_Method AUTO_INCREMENT = 1;
+delete from _C5VT_ClassDefinition;
+alter table _C5VT_ClassDefinition AUTO_INCREMENT = 1;
 delete from _C5VT_Class;
 alter table _C5VT_Class AUTO_INCREMENT = 1;
 delete from _C5VT_Function;
