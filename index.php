7cd75d82eb55aefc6b0eaaba1453b8bb19bc4f09
Michele Locati <michele@locati.it>
2014-03-16 12:38:56 +0100

    Maximize width of source code viewer

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
 </div>
 
 <div id="dialog-definitions" class="modal" tabindex="-1">
-	<div class="modal-dialog modal-lg">
+	<div class="modal-dialog modal-lg" style="width: 95%">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
